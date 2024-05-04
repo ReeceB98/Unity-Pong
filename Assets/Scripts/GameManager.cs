049a434d76fefb8ba9b52f8a7a7b2d917627d5b7
Reece <reecebellchambers98@gmail.com>
2024-05-04 18:44:16 +0100

    Game can now be paused using a singular toggle button in input system.
    "P" is the button you toggle for pause.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     private Ball ball = null;
+    private InputSystem inputSystem = null;
 
     [SerializeField] private Text player1WinsText;
     [SerializeField] private Text player2WinsText;
@@ -11,9 +13,22 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Button tryAgainButton;
     [SerializeField] private Button menuButton;
 
+    private bool pausingGame = false;
+
+    private void Awake()
+    {
+        inputSystem = new InputSystem();
+    }
+
     private void Start()
     {
         ball = FindObjectOfType<Ball>();
+    }
+
+    private void OnEnable()
+    {
+        inputSystem.Enable();
+        inputSystem.GameMenu.Pause.started += PauseGame;
     }
 
     public void DisplayPlayer1Wins()
@@ -33,5 +48,21 @@ public class GameManager : MonoBehaviour
     {
         tryAgainButton.gameObject.SetActive(true);
         menuButton.gameObject.SetActive(true);
+    }
+
+    private void PauseGame(InputAction.CallbackContext value)
+    {
+        // Pauses the game.
+        if (value.started && !pausingGame)
+        {
+            Time.timeScale = 0.0f;
+            pausingGame = true;
+        }
+        // Unpauses the game.
+        else if (value.started && pausingGame)
+        {
+            Time.timeScale = 1.0f;
+            pausingGame = false;
+        }
     }
 }
